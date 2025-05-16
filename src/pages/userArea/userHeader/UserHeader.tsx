272e643b03fcc3493ac179cc8e83d9673c0d21aa
JVCarv41 <jvc.04.off@gmail.com>
2025-05-16 12:54:43 -0300

    Bugfix in import

--- src/pages/userArea/userHeader/UserHeader.tsx
+++ src/pages/userArea/userHeader/UserHeader.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "./userHeader.css";
+import "./UserHeader.css";
 
 function UserHeader() {
   const navigate = useNavigate();
